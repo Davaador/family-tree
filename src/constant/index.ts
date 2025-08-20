4269caefae373d15b73193362a338938a2a321b0
daaduu <davaadorj.0510@gmail.com>
2025-08-20 11:59:32 +0800

    update

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,5 +1,5 @@
 const TOKEN_KEY = 'token';
 const USER_KEY = 'user';
-const API_URL = 'http://localhost:8081';
+const API_URL = 'http://51.20.251.210:8080';
 
 export { TOKEN_KEY, USER_KEY, API_URL };
