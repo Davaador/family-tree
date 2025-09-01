109c03f0357d8d4393ea5a2a70060b48d08e46e1
daaduu <davaadorj.0510@gmail.com>
2025-09-01 13:12:56 +0800

    update

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,6 +1,6 @@
 const TOKEN_KEY = 'token';
 const USER_KEY = 'user';
-const API_URL = 'http://localhost:8080';
-//const API_URL = 'https://api.urag.mn';
+// const API_URL = 'http://localhost:8080';
+const API_URL = 'https://api.urag.mn';
 
 export { TOKEN_KEY, USER_KEY, API_URL };
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,8 @@
 // Legacy exports for backward compatibility
 const TOKEN_KEY = 'token';
 const USER_KEY = 'user';
-const API_URL = 'http://localhost:8080';
-//const API_URL = 'https://api.urag.mn';
+// const API_URL = 'http://localhost:8080';
+const API_URL = 'https://api.urag.mn';
 
 export { TOKEN_KEY, USER_KEY, API_URL };
 
